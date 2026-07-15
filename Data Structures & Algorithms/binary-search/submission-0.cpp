17bd268bbdd498ef18d8b80f40cbb405409c00f1
Eswarpulavarthi <151733249+Eswarpulavarthi@users.noreply.github.com>
2026-07-15 12:19:32 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int left, int right, int target) {
+
+        if (left > right)
+            return -1;
+
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[mid] < target)
+            return binarySearch(nums, mid + 1, right, target);
+
+        return binarySearch(nums, left, mid - 1, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, 0, nums.size() - 1, target);
+    }
+};
